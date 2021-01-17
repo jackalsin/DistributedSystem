a0a65fb0230d3f2b90e6db7bcaf2a56b240c0c69
Zhiwei Xin <zhixi@microsoft.com>
2021-01-16 16:46:27 -0800

    Upgrade to 11

---
+++ src/main/java/paramTest/Container.java
@@ -0,0 +1,17 @@
+package paramTest;
+
+/**
+ * @author zhixi
+ * @version 1.0 on 1/16/2021
+ */
+public final class Container {
+  private final int val;
+
+  public Container(int val) {
+    this.val = val;
+  }
+
+  public int val() {
+    return val;
+  }
+}
